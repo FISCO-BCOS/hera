6b39ecdc437140c01044a69e09f2e34adb6b6a43
bxq2011hust <bxq2011hust@qq.com>
2021-06-22 20:24:05 +0800

    use hunter to compile evmc

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,8 @@
+# evmone: Fast Ethereum Virtual Machine implementation
+# Copyright 2018-2019 The evmone Authors.
+# Licensed under the Apache License, Version 2.0.
+
+hunter_config(evmc VERSION 7.3.0-d951b1ef
+	URL https://github.com/FISCO-BCOS/evmc/archive/d951b1ef088be6922d80f41c3c83c0cbd69d2bfa.tar.gz
+	SHA1 0b39b36cd8533c89ee0b15b6e94cff1111383ac7
+)
--- cmake/Hunter/init.cmake
+++ cmake/Hunter/init.cmake
@@ -5,6 +5,7 @@ set(HUNTER_CONFIGURATION_TYPES Release
 include(HunterGate)
 
 HunterGate(
-    URL "https://github.com/ruslo/hunter/archive/v0.23.169.tar.gz"
-    SHA1 "60de9f275143ab38df559986c04e3726683e7de7"
+    URL "https://github.com/FISCO-BCOS/hunter/archive/f88ce89a9fcd557b7fb2aa0f7abaa3c541dc6903.tar.gz"
+    SHA1 "b7792231bf64539602e696e32bfc163c9bc1812a"
+    LOCAL
 )
